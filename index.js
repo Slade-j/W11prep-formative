687e8476a714e13bf16f39a9d0303c968cbd443f
Jason Slade <jason.slade906@gmail.com>
2021-01-24 00:35:58 -0700

    seting up post to fill out table. drop mixin for specs.

--- index.js
+++ index.js
@@ -1,4 +1,6 @@
 const express = require("express");
+const cookieParser = require("cookie-parser");
+const csrf = require("csurf");
 
 
 const app = express();
@@ -6,7 +8,20 @@ const port = process.env.PORT || 3000;
 
 
 app.set("view engine", "pug");
+app.use(express.urlencoded({ extended: true }));
+app.use(cookieParser({ cookie: true }));
+const csrfProtection = csrf( {cookie: true });
 
+const validations = (req, res, next) => {
+  const errors = [];
+  const { firstName, LastName, email, password, confirmedPassword } = req.body;
+
+  if (!firstName) errors.push("Please provide a first name.");
+  if (!lastName) errors.push("Please provide a last name")
+
+  req.errors = errors;
+  next();
+}
 
 
 const users = [
@@ -22,9 +37,22 @@ app.get("/", (req, res) => {
   res.render("index", { users });
 });
 
-app.get("/create", (req, res) => {
-  res.render("create-normal")
-})
+app.get("/create", csrfProtection, (req, res) => {
+  res.render("create-normal", { csrfToken: req.csrfToken() })
+});
+
+app.post("/create", validations, (req, res) => {
+  const { firstName, lastName, email, password, confirmedPassword } = req.body;
+  const returnData = {errors: req.errors, firstName, lastName, email, password, confirmedPassword };
+
+  if (req.errors.length) {
+    res.render("create-normal", returnData);
+  } else {
+    const currId = users[users.length - 1].id;
+    users.push({ id: currId + 1, firstName, lastName, email });
+    res.redirect('/');
+  }
+});
 
 app.listen(port, () => console.log(`Example app listening on port ${port}!`));
 
